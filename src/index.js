e2e3b613646c8a0fa172b9137bc97990b1c0ea2d
12metalx <perezozo79@gmail.com>
2021-04-14 00:11:06 -0500

    Eliminacion de variables inecesarias

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import ReactDOM from "react-dom";
-import Contador from "./Contador";
 import Form from "./Form";
 import "./scss/main.scss";
 
-const button = document.getElementById("init");
 
 ReactDOM.render(<Form/>,document.getElementById("app"));
 
